797087cf20c89a9942bdf44a4753f6c6e6cf0bc0
calmzhu <saint@justcalm.org>
2020-01-03 10:45:05 +0800

    Add and configure flake8 as code syntax check

--- configer/__init__.py
+++ configer/__init__.py
@@ -3,5 +3,4 @@ from .ymlConfiger import YmlConfiger
 VERSION = "0.1.0"
 VERSION_IS_SNAP = True
 
-
 __all__ = ['YmlConfiger']
--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,14 +24,12 @@ author = 'calmzhu'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +38,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
