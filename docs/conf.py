5cb418d6946fba36e7d8c6e9a74e3f699b962337
calmzhu <saint@justcalm.org>
2020-02-02 01:14:33 +0800

    Release v0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, calmzhu'
 author = 'calmzhu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.2'
 
 # -- General configuration ---------------------------------------------------
 
